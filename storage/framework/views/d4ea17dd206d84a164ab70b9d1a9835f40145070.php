9c0f281b5b9b423b1214a84b137c20f3ba31aa21
catartista <161763483+catartista@users.noreply.github.com>
2024-03-06 10:07:12 +0400

    Add files via upload

---
+++ storage/framework/views/d4ea17dd206d84a164ab70b9d1a9835f40145070.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+      
+    </head>
+    <body class="antialiased">
+        <h1>Welcome to artista</h1>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script src="<?php echo e(mix('js/app.js')); ?>" ></script>
+    </body>
+</html>
+<?php /**PATH C:\Program Files\Ampps\www\naimu\artista\resources\views/welcome.blade.php ENDPATH**/ ?>
